75873b5d18c1494d2be7dff6a2e1023f225e2fc2
SuryaBoggarapu <srteja716@gmail.com>
2020-02-08 13:52:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/roombooking1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.roombooking1;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_admnlogin,btn_userlogin;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Dashboard");
+
+
+
+
+    }
+}
